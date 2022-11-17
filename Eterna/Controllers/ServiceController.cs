b611791fbdd0e5a6a0a2bd17871c9acf64db44d2
Lmntria <seymur1095@gmail.com>
2022-11-18 03:14:31 +0400

    service page added

---
+++ Eterna/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eterna.Models;
+using Eterna.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Eterna.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            HomeVm vm = new HomeVm
+            {
+                Services=Data.Services
+            };
+            return View(vm);
+        }
+    }
+}
+
